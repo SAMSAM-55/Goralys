9cbdba990d5e05944d90cfab8d80dcde95635e1c
SAMSAM55 <sami.saubion@gmail.com>
2026-01-18 11:42:00 +0100

    Added: cross-origin control headers

--- backend/src/Kernel/bootstrap.php
+++ backend/src/Kernel/bootstrap.php
@@ -5,6 +5,11 @@ use Goralys\Kernel\GoralysKernel;
 // --------------- Kernel Init --------------- //
 function bootKernel(bool $useFlash = false, bool $test = false, array $files = []): GoralysKernel
 {
+    header("Access-Control-Allow-Origin: https://goralys.fr");
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    header('Access-Control-Allow-Credentials: true');
+    header("Access-Control-Max-Age: 84600"); // cache for a day
+
     $kernel = new GoralysKernel(__DIR__ . "/../../", $useFlash, $test, $files);
     $kernel->setHandlers();
     return $kernel;
