df6cc50caf5db526ef35b256425a18a1113e8d90
SAMSAM55 <sami.saubion@gmail.com>
2026-01-18 12:18:47 +0100

    Added: API bootstrap method

--- backend/src/Kernel/bootstrap.php
+++ backend/src/Kernel/bootstrap.php
@@ -2,13 +2,33 @@
 
 use Goralys\Kernel\GoralysKernel;
 
+// ----------- API bootstrap method ---------- //
+function bootstrapAPI(): void
+{
+
+    $origin = $_SERVER['HTTP_ORIGIN'] ?? '';
+
+    if ($origin === 'https://goralys.fr') {
+        header('Access-Control-Allow-Origin: https://goralys.fr');
+        header('Access-Control-Allow-Credentials: true');
+        header('Vary: Origin');
+    }
+
+    header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+    header('Access-Control-Max-Age: 86400'); // 1 day
+    header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+
+    // Preflight requests
+    if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+        http_response_code(204); // No content, but OK
+        exit;
+    }
+}
+
 // --------------- Kernel Init --------------- //
 function bootKernel(bool $useFlash = false, bool $test = false, array $files = []): GoralysKernel
 {
-    header("Access-Control-Allow-Origin: https://goralys.fr");
-    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-    header('Access-Control-Allow-Credentials: true');
-    header("Access-Control-Max-Age: 84600"); // cache for a day
+    bootStrapAPI();
 
     $kernel = new GoralysKernel(__DIR__ . "/../../", $useFlash, $test, $files);
     $kernel->setHandlers();
