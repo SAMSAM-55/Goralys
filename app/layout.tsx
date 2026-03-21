9e6e5f79ff3f2cd16fd69483571bf7fb21cfc872
SAMSAM5.5 <sami.saubion@gmail.com>
2026-03-22 00:49:47 +0100

    fix: ui bug with block for non-desktop devices

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,13 @@ export default function RootLayout({children}: Readonly<{children: React.ReactNo
         <title>Goralys</title>
     </head>
 
-    <body className={`${lusitana.className} antialiased text-gray-900 bg-gray-50`}>
+    <body className={`${lusitana.className} antialiased text-gray-900 bg-gray-50 lg:overflow-auto overflow-hidden`}>
+
+    <div className="z-50 absolute h-screen w-screen lg:hidden bg-gray-200 flex items-center justify-center p-6 overflow-hidden">
+        <h1 className="font-bold text-center text-2xl">
+            Goralys n&#39;est malheureusement pas disponible sur téléphone et tablette pour l&#39;instant.
+        </h1>
+    </div>
 
     <div id="toast-root"></div>
     <div id="confirm-root"></div>
