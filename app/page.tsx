81beedd57b3c8b24ec1af938d5314afe917341ef
opinson <51924614+opinson@users.noreply.github.com>
2026-04-03 18:28:11 +0200

    Update page.tsx
    ajout de l'affiche sur la page d'accueil

--- app/page.tsx
+++ app/page.tsx
@@ -14,45 +14,58 @@ import Image from "next/image";
 
 export default function Home() {
     return (
-        <div className="flex flex-col min-h-screen font-sans w-full">
+        <div className="flex min-h-screen w-full">
 
-            <div className="self-center w-3/4 mt-11">
-                <h1 className="font-bold text-xl mb-5">
-                    Bienvenue sur Goralys,
-                </h1>
+            {/* PARTIE GAUCHE */}
+            <div className="flex flex-col justify-between w-1/2 p-10">
 
-                <p className="mb-2.5">
-                    L&#39;application de gestion du grand oral au lycée Auguste et Jean Renoir.
-                </p>
+                <div>
+                    <h1 className="font-bold text-xl mb-5">
+                        Bienvenue sur Goralys,
+                    </h1>
 
-                <p>
-                    Cette plateforme a été entièrement développée par Sami Saubion, élève du lycée.
-                </p>
+                    <p className="mb-2.5">
+                        L&#39;application de gestion du Grand Oral au lycée Auguste et Jean Renoir.
+                    </p>
+
+                    <p>
+                        Cette plateforme a été entièrement développée par Sami Saubion, élève du lycée.
+                    </p>
+                </div>
+
+                <div>
+                    <Image
+                        src="/logo/logo_renoir.png"
+                        alt="Logo du lycée"
+                        width={75}
+                        height={75}
+                    />
+
+                    <footer className="text-xs opacity-70 mt-4">
+                        © 2026 Sami Saubion — AGPL-3.0 —
+                        <a
+                            href="https://github.com/SAMSAM-55/Goralys"
+                            className="underline ml-1"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            Source code
+                        </a>
+                    </footer>
+                </div>
             </div>
 
-            <div className="flex grow" />
-
-            <div className="flex flex-col items-start gap-2 mb-4 ml-4">
-                <Image
-                    src="/logo/logo_renoir.png"
-                    alt="Logo du lycée Auguste et Jean Renoir"
-                    width={75}
-                    height={10}
-                />
-
-                <footer className="text-xs opacity-70 self-center">
-                    © 2026 Sami Saubion — AGPL-3.0 —
-                    <a
-                        href="https://github.com/SAMSAM-55/Goralys"
-                        className="underline ml-1"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Source code
-                    </a>
-                </footer>
-            </div>
-
-        </div>
+            {/* PARTIE DROITE (IMAGE) */}
+            <div className="flex items-center justify-center w-2/5 h-screen">
+				<div className="relative w-[90%] h-[90%]">
+					<Image
+						src="/logo/affiche.png"
+						alt="affiche grand oral"
+						fill
+						className="object-contain"
+					/>
+			</div>
+		</div>
+    </div>
     );
 }
