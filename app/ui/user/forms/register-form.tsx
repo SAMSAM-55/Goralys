4719aa5f623f4b815efd7bbcff0b05fcec13023d
SAMSAM5.5 <sami.saubion@gmail.com>
2026-05-01 01:36:35 +0200

    remove: old API

--- app/ui/user/forms/register-form.tsx
+++ app/ui/user/forms/register-form.tsx
@@ -6,7 +6,7 @@ import {fetchCsrfClient} from "@/app/lib/fetch/fetch.client";
 
 export default function RegisterForm() {
     const [csrfToken, setCsrfToken] = useState<string | null>(null);
-    const requestUrl = `${process.env.NEXT_PUBLIC_API_DOMAIN}/User/Auth/Register/`
+    const requestUrl = `${process.env.NEXT_PUBLIC_API_DOMAIN}/user/register`
 
     useEffect(() => {
         const run = async () => setCsrfToken(await fetchCsrfClient("register"));
